ac60d8b337020ec72b374708a070f1d5eec86e88
at0ms <153468047+at0ms@users.noreply.github.com>
2026-04-03 20:03:34 +0100

    Updated pkgi.c, sfo.xml

--- source/pkgi.c
+++ source/pkgi.c
@@ -271,7 +271,7 @@ static void pkgi_do_main(pkgi_input* input)
         {
             input->pressed &= ~PKGI_BUTTON_SELECT;
             pkgi_dialog_message("\xE2\x98\x85  PKGi v" PKGI_VERSION "  \xE2\x98\x85", 
-                                    "             PlayStation 3 version by Bucanero (Fork by at0ms)\n\n"
+                                    "                 PKGi Fork by at0ms\n\n"
                                     "           https://github.com/at0ms/pkgi-ps3/");
         }
 
@@ -386,7 +386,7 @@ static void pkgi_do_main(pkgi_input* input)
 
         if (i == selected_item)
         {
-            pkgi_draw_fill_rect_z(0, y + 1, PKGI_FONT_Z, VITA_WIDTH, font_height + PKGI_MAIN_ROW_PADDING - 1, PKGI_COLOR_SELECTED_BACKGROUND);
+            pkgi_draw_fill_rect_z(0, y - 2, PKGI_FONT_Z, VITA_WIDTH, font_height + PKGI_MAIN_ROW_PADDING - 1, PKGI_COLOR_SELECTED_BACKGROUND);
         }
         uint32_t color = PKGI_COLOR_TEXT;
 
